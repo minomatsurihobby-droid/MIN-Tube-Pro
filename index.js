7d43148e89c95c4624edc12187fd53974f149be6
minomatsurihobby-droid <mino.matsuri.hobby@gmail.com>
2026-05-23 21:54:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+
 app.get("/api/trending", async (req, res) => {
   const page = parseInt(req.query.page) || 0;
   try {
